6fa68a1a938cb659e0e3bf46e068f9760c28354f
Ilya Avdeev <webjsd3v@yandex.ru>
2021-10-16 01:54:25 +0300

    feat:  Add js

---
+++ scripts/index.js
@@ -0,0 +1,131 @@
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const profile = document.querySelector(".profile");
+const profileTitle = profile.querySelector(".profile__title");
+const profileSubtitle = profile.querySelector(".profile__subtitle");
+const popUpEditProfile =  document.querySelector(".popup_type_edit-profile");
+const popUpOpenEdit = profile.querySelector(".profile__edit-button");
+const popUpCloseEdit = popUpEditProfile.querySelector(".popup__close-btn");
+const formEdit = popUpEditProfile.querySelector(".popup__form");
+const nameInput = formEdit.inputTitle;
+const jobInput = formEdit.inputSubtitle;
+const elements = document.querySelector(".elements");
+const templateElement = document.querySelector(".element-template").content;
+const popUpAddElement = document.querySelector(".popup_type_add-element");
+const popUpOpenAdd = profile.querySelector(".profile__add-button");
+const popUpCloseAdd = popUpAddElement.querySelector(".popup__close-btn");
+const formAdd = popUpAddElement.querySelector(".popup__form");
+const nameInputAdd = formAdd.inputTitleAdd;
+const urlInputAdd = formAdd.inputUrlAdd;
+const popUpImageElement = document.querySelector(".popup_type_image");
+const popUpImage = popUpImageElement.querySelector(".popup__figure-image");
+const popUpImageCaption = popUpImageElement.querySelector(".popup__caption");
+const popUpImageClose = popUpImageElement.querySelector(".popup__close-btn");
+
+const previewImage = (name, link) => {
+  popUpImage.src = link;
+  popUpImage.alt = `Фотография ${name}`;
+  popUpImageCaption.textContent = name;
+  openPopUp(popUpImageElement);
+}
+
+function openPopUp(arg){
+  arg.classList.add("popup_opened");
+}
+
+function closePopUp(arg){
+  arg.classList.remove("popup_opened");
+}
+
+popUpOpenEdit.addEventListener("click", () => {
+  nameInput.value = profileTitle.textContent;
+  jobInput.value = profileSubtitle.textContent;
+  openPopUp(popUpEditProfile);
+})
+
+popUpCloseEdit.addEventListener("click", () => {
+  closePopUp(popUpEditProfile);
+})
+
+formEdit.addEventListener("submit",(evt)=>{
+  evt.preventDefault();
+  profileTitle.textContent = nameInput.value;
+  profileSubtitle.textContent = jobInput.value;
+  closePopUp(popUpEditProfile);
+})
+
+function addElement(name, link){
+  const elementItem = templateElement.querySelector(".element").cloneNode(true);
+  const imageElement = elementItem.querySelector(".element__image");
+  const titleElement = elementItem.querySelector(".element__title");
+
+  titleElement.textContent = name;
+  imageElement.src = link;
+  imageElement.alt = `Фотография ${name}`;
+
+  const buttonTrash = elementItem.querySelector(".element__button-trash");
+  buttonTrash.addEventListener("click",(evt) => {
+    evt.target.closest("article.element").remove();
+  })
+
+  const buttonLike = elementItem.querySelector(".element__button-like");
+  buttonLike.addEventListener("click", () => {
+    buttonLike.classList.toggle("element__button-like_active");
+  })
+
+  imageElement.addEventListener("click", () => previewImage(name,link));
+
+  popUpImageClose.addEventListener("click", () => {
+    closePopUp(popUpImageElement)
+  })
+
+  return elementItem;
+}
+
+formAdd.addEventListener("submit",(evt)=>{
+  evt.preventDefault();
+  elements.prepend(addElement(nameInputAdd.value, urlInputAdd.value))
+  closePopUp(popUpAddElement);
+})
+
+
+popUpOpenAdd.addEventListener("click",() => {
+  openPopUp(popUpAddElement);
+})
+
+popUpCloseAdd.addEventListener("click", () => {
+  closePopUp(popUpAddElement)
+})
+
+function render(){
+  initialCards.forEach((arg)=>{
+    elements.append(addElement(arg.name, arg.link))
+  })
+}
+
+render()
