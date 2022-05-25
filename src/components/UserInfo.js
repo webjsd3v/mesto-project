9d719d3070a16156d499f0e2c987d58a8f6c23f6
wellear <rexserwolve@mail.ru>
2022-05-25 12:13:52 +0300

    added Userinfo
     Userinfo.

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,5 +1,21 @@
 class UserInfo{
-  constructor() {
+  constructor({ nameInput, aboutInput, avatarLink }) {
+    this._nameInput = document.querySelector(nameInput);
+    this._aboutInput = document.querySelector(aboutInput);
+    this._avatarLink = document.querySelector(avatarLink);
+  }
+  getUserInfo() {
+    return {
+      name: this._nameInput.textContent,
+      about: this._aboutInput.textContent,
+      avatarLink: this._avatarLink.src
+    }
+  }
+
+  setUserInfo(data) {
+    this._nameInput.textContent = data.name;
+    this._aboutInput.textContent = data.about;
+    this._avatarLink.src = data.avatar;
   }
 }
 
