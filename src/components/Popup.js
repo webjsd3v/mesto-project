58687180a704259adf73d55f3656451ba8b01455
wellear <rexserwolve@mail.ru>
2022-05-28 10:21:37 +0300

    Edit  Popup & PopupWithForm
    Не линковал с Index.js
    Не линковал с Index.js

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,28 +1,35 @@
-class Popup{
+class Popup {
   constructor(popup) {
     this._elementPopup = document.querySelector(popup);
     this._handleEsc = this._handleEsc.bind(this);
   }
 
-  _handleEsc(event){
+  _handleEsc(event) {
     event.preventDefault();
-    if (event.key === 'Escape'){
-      this.close()
+    if (event.key === "Escape") {
+      this.close();
     }
   }
 
-  open(){
+  open() {
     this._elementPopup.classList.add("popup_opened");
-    document.addEventListener('keydown', this._handleEsc)
+    document.addEventListener("keydown", this._handleEsc);
   }
 
-  close(){
-    this._elementPopup.classList.remove("popup_opened");
-    document.removeEventListener('keydown', this._handleEsc);
+  close() {
+    this._elementPopup.classList.remove("popup_closed");
+    document.removeEventListener("keydown", this._handleEsc);
   }
 
-  setEventListener(){
-
+  setEventListeners() {
+    this._popup.addEventListener("mousedown", (evt) => {
+      if (
+        evt.target.classList.contains("popup_opened") ||
+        evt.target.classList.contains("popup__closed")
+      ) {
+        this.close();
+      }
+    });
   }
 }
 
--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,11 +1,36 @@
 import Popup from "./Popup";
 
-class PopupWithForm extends Popup{
-  constructor(props) {
-    super(props);
-
+class PopupWithForm extends Popup {
+  constructor(popup, form, handleFormSubmit) {
+    super(popup);
+    this._handleFormSubmit = handleFormSubmit;
+    this._form = form;
+    this._popupSubmit = this._form.querySelector(".popup__button");
+    this._inputs = this._form.querySelector(".popup__input");
   }
-
+  _getInputValues() {
+    this._formValue = {};
+    this._inputs.forEach((input) => {
+      this._formValue[input.name] = input.value;
+    });
+    return this_formValue;
+  }
+  setInputValues(data) {
+    this._inputs.forEach((input) => {
+      input.value = data[input.name];
+    });
+  }
+  setEventListener() {
+    super.setEventListener();
+    this._form.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+      this._handleFormSubmit(this._getInputValues());
+    });
+  }
+  close() {
+    super.closePopup();
+    this._form.reset();
+  }
 }
 
 export default PopupWithForm;
