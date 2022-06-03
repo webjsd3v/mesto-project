4b3e6472e8f0dfb122d06b9412ed4396fb9c9d7c
Ilya Avdeev <webjsd3v@yandex.ru>
2022-06-03 22:16:54 +0300

    Revert "Merge pull request #8 from webjsd3v/feat/update"
    This reverts commit 09a7c4c46d594b105ed7269063accd0304d8eb9d, reversing
    changes made to 40427dabddf73358cb1be5ce065891ae8f6560ba.

---
+++ src/components/card.js
@@ -0,0 +1,73 @@
+import {templateElement} from '../utils/constants';
+import {idUser} from "./index";
+import {openImage} from './modal'
+import {addLikeElement, delApiElement, removeLikeElement} from "./api";
+
+function addElement(elements){
+  const elementItem = templateElement.querySelector('.element').cloneNode(true);
+  const imageElement = elementItem.querySelector('.element__image');
+  const titleElement = elementItem.querySelector('.element__title');
+  const numberLike = elementItem.querySelector('.element__count');
+  const buttonLike = elementItem.querySelector('.element__button-like');
+  const buttonTrash = elementItem.querySelector(".element__button-trash");
+
+  titleElement.textContent = elements.name;
+  imageElement.src = elements.link;
+  imageElement.alt = `Фотография ${elements.name}`;
+  numberLike.textContent = elements.likes.length;
+  buttonTrash.elements = elements._id;
+
+  if (idUser !== elements.owner._id){
+    buttonTrash.remove()
+  }else{
+    buttonTrash.addEventListener("click",() => {
+      delApiElement(buttonTrash.elements)
+        .then(() => elementItem.remove())
+        .catch(err => console.log(err))
+    })
+  }
+
+  if(elements.likes.find(like => like._id === idUser)){
+    buttonLike.classList.add("element__button-like_active");
+  }else {
+    buttonLike.classList.remove("element__button-like_active");
+  }
+
+  buttonLike.addEventListener("click", () => {
+    if(elements.likes.find(like => like._id === idUser)){
+      removeLikeElement(elements._id)
+        .then(elementData => {
+          numberLike.textContent = elementData.likes.length;
+          elements.likes = elementData.likes;
+          buttonLike.classList.remove("element__button-like_active");
+        })
+        .catch(err => console.log(err))
+    }else {
+      addLikeElement(elements._id)
+        .then(elementData => {
+          numberLike.textContent = elementData.likes.length;
+          elements.likes = elementData.likes;
+          buttonLike.classList.add("element__button-like_active");
+        })
+        .catch(err => console.log(err))
+    }
+  })
+
+  imageElement.addEventListener("click", () => openImage(elements.name,elements.link));
+
+  return elementItem;
+}
+
+function renderCards(section, element, method){
+  if (method === 'POST'){
+    section.prepend(element)
+  }else {
+    section.append(element)
+  }
+
+}
+
+export {
+  addElement,
+  renderCards
+}
