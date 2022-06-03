0217f8f9be494b264e1802f1df5f8717443d0791
Ilya Avdeev <webjsd3v@yandex.ru>
2022-06-03 22:26:35 +0300

    Revert "Revert "Merge pull request #8 from webjsd3v/feat/update""
    This reverts commit 4b3e6472e8f0dfb122d06b9412ed4396fb9c9d7c.

--- src/components/api.js
+++ src/components/api.js
@@ -1,83 +1,89 @@
-import {apiConfigOptions} from "../utils/constants";
-
-function checkApiError(res){
-  if (res.ok){
-    return res.json()
+class Api{
+  constructor(apiConfigOptions) {
+    this._apiConfigOptions = apiConfigOptions
   }
-  return Promise.reject(`Ошибка сервера -- ${res.status}`)
-}
 
-export function addApiElement(nameElement, linkElement){
-  return fetch(`${apiConfigOptions.baseUrl}/cards`,{
-    method: "POST",
-    headers: apiConfigOptions.headers,
-    body: JSON.stringify({
-      name: nameElement,
-      link: linkElement
+  _checkApiError(res){
+    if (res.ok){
+      return res.json()
+    }
+    return Promise.reject(`Ошибка сервера -- ${res.status}`)
+  }
+
+  addApiElement(nameElement, linkElement){
+    return fetch(`${this._apiConfigOptions.baseUrl}/cards`,{
+      method: "POST",
+      headers: this._apiConfigOptions.headers,
+      body: JSON.stringify({
+        name: nameElement,
+        link: linkElement
+      })
     })
-  })
-    .then(res => checkApiError(res))
-}
+      .then(this._checkApiError)
+  }
 
-export function getElements(){
-  return fetch(`${apiConfigOptions.baseUrl}/cards`,{
-    method: "GET",
-    headers: apiConfigOptions.headers
-  })
-    .then(res => checkApiError(res))
-}
-
-export function delApiElement(idElement){
-  return fetch(`${apiConfigOptions.baseUrl}/cards/${idElement}`,{
-    method: 'DELETE',
-    headers: apiConfigOptions.headers
-  })
-    .then(res => checkApiError(res))
-}
-
-export function addLikeElement(idlike){
-  return fetch(`${apiConfigOptions.baseUrl}/cards/likes/${idlike}`,{
-    method: "PUT",
-    headers: apiConfigOptions.headers
-  })
-    .then(res => checkApiError(res))
-}
-
-export function removeLikeElement(idlike){
-  return fetch(`${apiConfigOptions.baseUrl}/cards/likes/${idlike}`,{
-    method: "DELETE",
-    headers: apiConfigOptions.headers
-  })
-    .then(res => checkApiError(res))
-}
-
-export function getUserProfile(){
-  return fetch(`${apiConfigOptions.baseUrl}/users/me`,{
-    method: "GET",
-    headers: apiConfigOptions.headers
-  })
-    .then(res => checkApiError(res))
-}
-
-export function addUserProfileInfo(profileName, profileSubtitle){
-  return fetch(`${apiConfigOptions.baseUrl}/users/me`,{
-    method: "PATCH",
-    headers: apiConfigOptions.headers,
-    body: JSON.stringify({
-      name: profileName,
-      about: profileSubtitle
+  getElements(){
+    return fetch(`${this._apiConfigOptions.baseUrl}/cards`,{
+      method: "GET",
+      headers: this._apiConfigOptions.headers
     })
-  })
-    .then(res => checkApiError(res))
+      .then(this._checkApiError)
+  }
+
+  delApiElement(idElement){
+    return fetch(`${this._apiConfigOptions.baseUrl}/cards/${idElement}`,{
+      method: 'DELETE',
+      headers: this._apiConfigOptions.headers
+    })
+      .then(this._checkApiError)
+  }
+
+  addLikeElement(idlike){
+    return fetch(`${this._apiConfigOptions.baseUrl}/cards/likes/${idlike}`,{
+      method: "PUT",
+      headers: this._apiConfigOptions.headers
+    })
+      .then(this._checkApiError)
+  }
+
+  removeLikeElement(idlike){
+    return fetch(`${this._apiConfigOptions.baseUrl}/cards/likes/${idlike}`,{
+      method: "DELETE",
+      headers: this._apiConfigOptions.headers
+    })
+      .then(this._checkApiError)
+  }
+
+  getUserProfile(){
+    return fetch(`${this._apiConfigOptions.baseUrl}/users/me`,{
+      method: "GET",
+      headers: this._apiConfigOptions.headers
+    })
+      .then(this._checkApiError)
+  }
+
+  addUserProfileInfo(profileName, profileSubtitle){
+    return fetch(`${this._apiConfigOptions.baseUrl}/users/me`,{
+      method: "PATCH",
+      headers: this._apiConfigOptions.headers,
+      body: JSON.stringify({
+        name: profileName,
+        about: profileSubtitle
+      })
+    })
+      .then(this._checkApiError)
+  }
+
+  addUserProfileAvatar(userAvatar){
+    return fetch(`${this._apiConfigOptions.baseUrl}/users/me/avatar`,{
+      method: "PATCH",
+      headers: this._apiConfigOptions.headers,
+      body: JSON.stringify({
+        avatar: userAvatar
+      })
+    })
+      .then(this._checkApiError)
+  }
 }
 
-export function addUserProfileAvatar(userAvatar){
-  return fetch(`${apiConfigOptions.baseUrl}/users/me/avatar`,{
-    method: "PATCH",
-    headers: apiConfigOptions.headers,
-    body: JSON.stringify({
-      avatar: userAvatar
-    })
-  })
-    .then(res => checkApiError(res))
-}
+export default Api
